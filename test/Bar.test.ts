6d655bf3cd0f17ce6600b5e9ba0a824c5bc26fa4
vectronic <hello.vectronic@gmail.com>
2019-05-21 17:22:10 +0100

    docs: add document and test sample of function throwing an error

--- test/Bar.test.ts
+++ test/Bar.test.ts
@@ -16,4 +16,14 @@ describe('Bar test', () => {
     it('Bar dump works', () => {
         new Bar().dump();
     });
+
+    it('iHazError throws', () => {
+        expect(() => {
+            new Bar().iHazError(true);
+        }).toThrow();
+    });
+
+    it('iHazError does not throw', () => {
+        new Bar().iHazError(false);
+    });
 });
