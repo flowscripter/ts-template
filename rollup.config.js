dee59ff9ca03636e6e0694607e45454192861cc1
vectronic <hello.vectronic@gmail.com>
2019-07-28 14:21:05 +0100

    feat: move to common renovate config feat: add and document browser specific module output

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import builtins from 'rollup-plugin-node-builtins';
 import cleanup from 'rollup-plugin-cleanup';
 import commonjs from 'rollup-plugin-commonjs';
 import { eslint } from 'rollup-plugin-eslint';
@@ -7,34 +8,76 @@ import typescript from 'rollup-plugin-typescript2';
 import ts from 'typescript';
 import tempDir from 'temp-dir';
 
-module.exports = {
-    input: {
-        index: 'src/index.ts',
-        Foo: 'src/Foo.ts',
-        Bar: 'src/Bar.ts'
+module.exports = [
+    {
+        input: {
+            index: 'src/index.ts'
+        },
+        output: {
+            dir: 'dist',
+            format: 'es',
+            sourcemap: true
+        },
+        watch: {
+            include: 'src/**',
+        },
+        external: [
+            'path',
+            'fs',
+            'crypto'
+        ],
+        plugins: [
+            peerDepsExternal(),
+            eslint({
+                include: [
+                    'src/**/*.ts'
+                ]
+            }),
+            typescript({
+                typescript: ts,
+                useTsconfigDeclarationDir: true,
+                cacheRoot: `${tempDir}/.rpt2_cache`
+            }),
+            commonjs(),
+            resolve(),
+            cleanup({
+                extensions: ['ts']
+            })
+        ]
     },
-    output: {
-        dir: 'dist',
-        format: 'es',
-        sourcemap: true
-    },
-    watch: {
-        include: 'src/**',
-    },
-    plugins: [
-        peerDepsExternal(),
-        eslint({
-            include: [
-                'src/**/*.ts'
-            ]
-        }),
-        typescript({
-            typescript: ts,
-            useTsconfigDeclarationDir: true,
-            cacheRoot: `${tempDir}/.rpt2_cache`
-        }),
-        commonjs(),
-        resolve(),
-        cleanup({ extensions: ['ts'] })
-    ]
-};
+    {
+        input: {
+            browser: 'src/index.ts'
+        },
+        output: {
+            dir: 'dist',
+            format: 'es',
+            sourcemap: true
+        },
+        watch: {
+            include: 'src/**',
+        },
+        plugins: [
+            peerDepsExternal(),
+            eslint({
+                include: [
+                    'src/**/*.ts'
+                ]
+            }),
+            typescript({
+                typescript: ts,
+                useTsconfigDeclarationDir: true,
+                cacheRoot: `${tempDir}/.rpt2_cache`
+            }),
+            commonjs(),
+            builtins(),
+            resolve({
+                browser: true,
+                preferBuiltins: false
+            }),
+            cleanup({
+                extensions: ['ts']
+            })
+        ]
+    }
+];
